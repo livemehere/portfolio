9a2646c94648289c3a21bbcb2693b7ff3c3e0edc
KongTM <rhdxoals@gmail.com>
2021-02-05 17:07:29 +0900

    JS:arrowUp button

--- main.js
+++ main.js
@@ -40,6 +40,19 @@ const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll',()=>{
     home.style.opacity = 1 - window.scrollY/homeHeight;
-    
 });
 
+
+// arrow-up button
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll',()=>{
+   if(window.scrollY > homeHeight/2) {
+    arrowUp.classList.add('visible')
+   }else{
+    arrowUp.classList.remove('visible')
+   }
+});
+
+arrowUp.addEventListener('click',()=>{
+    scrollIntoView('#home')
+});
