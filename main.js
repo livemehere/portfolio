92c23828b63260f0c78734c8eb387f7d78dabe97
KongTM <rhdxoals@gmail.com>
2021-02-05 18:20:56 +0900

    JS:work filtering

--- main.js
+++ main.js
@@ -53,6 +53,32 @@ document.addEventListener('scroll',()=>{
    }
 });
 
+// arrow-up click event
 arrowUp.addEventListener('click',()=>{
     scrollIntoView('#home')
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(event)=>{
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    if(filter== null){
+        return;
+    }
+    projectContainer.classList.add('animation-out');
+    
+    setTimeout(()=>{
+        projects.forEach((project)=>{
+            console.log(project.dataset.type)
+            if(filter ==='*' || filter===project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('animation-out');
+    },300);
 });
